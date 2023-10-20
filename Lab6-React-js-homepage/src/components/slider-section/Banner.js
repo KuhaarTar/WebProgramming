7be000e3ea43b9858a71624bbbfc016933ef86bd
KuhaarTar <flempiefilms@gmail.com>
2023-10-20 11:19:37 +0300

    e-commerce on react

---
+++ Lab6-React-js-homepage/src/components/slider-section/Banner.js
@@ -0,0 +1,21 @@
+import banner from './../../img/Banner.png'
+import './scss/Banner.scss'
+import SliderDots from "./SliderDots";
+
+const Banner = () => {
+    return (
+        <div className={'banner-wrapper'}>
+            <div className="banner">
+                <img src={banner} alt="" className="banner"/>
+                <p className={'on-banner-text'}>
+                    <span>25% discount</span> <br/>
+                    all Paulo Coelho
+                    books!
+                </p>
+            </div>
+            <SliderDots/>
+        </div>
+    )
+}
+
+export default Banner
