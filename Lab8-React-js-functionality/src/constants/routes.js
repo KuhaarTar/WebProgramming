a53c47f1ef72f1037b30b864fa78a5c1bb6684c6
Taras Kuhar <119263226+KuhaarTar@users.noreply.github.com>
2023-11-10 00:33:46 +0200

    Delete Lab8-React-js-functionality directory

--- Lab8-React-js-functionality/src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const HOME = '/home';
-export const CATALOG = '/catalog';
-export const CART = '/cart';
-export const BOOK = '/book';
-export const LOGIN = '/login';
-export const REGISTER = '/reg';
